04f2b3a5a925c196b3556b330f514dc5502d284d
Juan Alvaro Martinez <juanalvaromartinez@gmail.com>
2022-07-07 15:51:52 -0430

    pasar código de javascript a next.js

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+import '../styles/global.css';
+import '../styles/code_r11.css';
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,186 @@
-import Head from 'next/head'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+import Image from 'next/image';
+import img01 from '../public/images/coder11-001.jpg';
+import img02 from '../public/images/coder11-002.gif';
+import img03 from '../public/images/coder11-003.png';
+import img04 from '../public/images/fundamentos-web.jpg';
+import imgHTML from '../public/images/html.jpg';
+import imgCSS from '../public/images/css.jpg';
+import imgJavaScript from '../public/images/javascript.jpg';
+import imgVisualStudioCode from '../public/images/visual-studio-code.jpg';
+import imgGitSCM from '../public/images/git-scm.png';
+import imgGitHub from '../public/images/github.png';
+import Head from 'next/head';
+import Layout from '../components/layout';
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+export default function RutadeAprendizaje() {
+    return (
+        <Layout>
+            <Head>
+                <title>Ruta de Aprendizaje Code__R11</title>
+            </Head>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <div id="inicioHeader" class="w3-container w3-red w3-center" >
+                <h1 class="w3-margin w3-jumbo">CODE__R11</h1>
+                <p class="w3-xlarge">Aprendizaje Activo</p>
+                <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Ruta de Aprendizaje 2022-2023</button>
+            </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <div id="aprendizajeActivo" class="w3-row-padding w3-padding-64 w3-container">
+                <div class="w3-content r11-content-right">
+                    <div class="w33-twothird r11-dosuno">
+                        <h1>Aprendizaje Activo</h1>
+                        <h5 class="w3-padding-32">¿Deseas convertirte en un profesional con experiencia en el
+                            desarrollo de aplicaciones Web? 
+                             <strong> Modern Web Development</strong>: Aquí vas a encontrar un plan de estudios actualizado para el
+                            aprendizaje del desarrollo web moderno.
+                        </h5>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+                        <p class="w3-text-grey">Tu CODE__R11 tiene un contenido de grado
+                            profesional donde se integra la disciplina de aprendizaje y la adopción de buenas
+                            prácticas tecnológicas. El sitio ideal para el fortalecimiento y desarrollo
+                            de las habilidades relevantes necesarias para crear y diseñar aplicaciones web de manera profesional.</p>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
+                    </div>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+                    <div class="w33-third w33-center r11-dosuno">
+                        <Image class="imgSecciones"
+                            src={img01}
+                            alt="Aprendizaje Activo" />
+                    </div>
+                </div>
+            </div>
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+            <div id="desarrolloWeb" class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
+                <div class="w3-content r11-content-left">
+                    <div class="w33-third w33-center r11-dosuno">
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+                        <Image class="imgSecciones"
+                            src={img02}
+                            alt="Ruta de Aprendizaje del desarrollo web" />
+                    </div>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+                    <div class="w33-twothird r11-dosuno">
+                        <h1>Ruta JavaScript 2022-2023: Desarrollo y Diseño Web</h1>
+                        <h5 class="w3-padding-32">Programa de aprendizaje activo para aprender y desarrollar
+                            las habilidades para crear sitios y aplicaciones web. </h5>
+                        <p>Recuerda esto, <em>"solo se tiene éxito
+                            cuando apoyas la escalera en la pared correcta"</em>. En un mundo
+                            tecnológico con cientos de herramientas y más de una docena de
+                            lenguajes de programación que emplean las empresas más importantes,
+                            <strong>¿sabes que tipo de tecnologías y que ecosistema de trabajo elegir?</strong>
+                            El <b>proyecto Code R11</b> fue ideado para resolver este dilema. Así que hemos
+                            elegido para ti un paquete de herramientas y el entorno
+                            de programación de JavaScript para tu aprendizaje activo como
+                            punto de partida.
+                        </p>
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+                    </div>
+                    <button onclick="ocultar('retoCoder18')" class="w3-button w3-left-align w3-green">
+                        Reto Coder-18
+                    </button>
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+                    <div id="retoCoder18" class="w3-hide w3-container">
+                        <h2>Desarrollador Web Profesional</h2>
+                        <p class="w3-text-grey"><strong>¿Quieres convertirte en un <b>desarrollador web</b> profesional en el menor
+                            tiempo
+                            posible?</strong> Tu reto, si decides aceptarlo..., consiste en mantener un <b>enfoque orientado al
+                                aprendizaje y prácticas constantes</b> de estos paquetes elegidos. Esta es la
+                            clave para dominar el <b>desarrollo y diseño web profesional</b>.
+                            Y, el principal objetivo es hacerlo en el menor tiempo posible. Esto significa
+                            un compromiso real y sostenido para convertirte en un desarrollador web profesional.
+                            Este reto no es difícil, pero si desafiante.</p>
+                        <p class="w3-text-grey">Sin embargo, requiere dedicación y compromiso. Así que
+                            tendrás que dedicarle un mínimo de 1000 horas de aprendizaje y práctica
+                            para cada ciclo de 6 meses. Entonces, si logras
+                            mantener este ritmo durante 18 meses, vas a conseguir alcanzar un excelente nivel
+                            de comprensión, competencias y dominio para el desarrollo y diseño web.</p>
+                        <p class="w3-text-grey">Esto significa que al finalizar tu primer ciclo de aprendizaje (de 6 a 8 meses)
+                            podrás ofrecer tus servicios como desarrollador web frontend. Y, los ciclos siguientes
+                            de aprendizaje te van a servir para escalar proyectos aun más demandantes y rentables.
+                        </p>
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+                    </div>
+                </div>
+            </div>
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+            <div id="ecosistema" class="w3-row-padding w3-padding-64 w3-container">
+                <div class="w3-content r11-content-right"  >
+                    <div class="w33-twothird r11-dosuno"  >
+                        <h1>JavaScript, Frameworks, Herramientas e Implementación</h1>
+                        <h5 class="w3-padding-32">El diseño y desarrollo de aplicaciones web profesional
+                            gira en torno al lenguaje programación JavaScript. Por lo tanto, el camino o ruta de
+                            aprendizaje inicial comienza con los fundamentos del Desarrollo Web, continua con
+                            el desarrollo web frontend y la implantación de un marco de desarrollo.
+                            Y, si escalas otro nivel, esta ruta te guiará hasta lo mejor del backend
+                            para completar tu ciclo de aprendizaje.
+                        </h5>
 
-        .title,
-        .description {
-          text-align: center;
-        }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+                    </div>
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
+                    <div class="w33-third w33-center r11-dosuno">
 
-          max-width: 800px;
-          margin-top: 3rem;
-        }
+                        <Image class="imgSecciones" 
+                        src={img03}  
+                        alt="Aprendizaje Activo" 
+                        />
+                    </div>
+                </div>
+            </div>
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+            {/* 1. Fundamentos */}
+            <div id="fundamentos" class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
+                <div class="w3-content r11-content-left">
+                    <div class="w33-third w33-center r11-dosuno">
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
+                        <Image 
+                            src={img04} 
+                            alt="fundamentos web" 
+                        />
+                    </div>
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
+                    <div class="w33-twothird r11-dosuno">
+                        <h2>1. Fundamentos para el Desarrollo web</h2>
+                        <div class="imgBloque1">
 
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
+                            <Image src={imgHTML} alt="HTML" />
+                            <Image src={imgCSS} alt="CSS" />
+                            <Image src={imgJavaScript} alt="JavaScript" />
 
-        .logo {
-          height: 1em;
-        }
+                        </div>
+                        <ul>
+                            <li><b>HTML</b>: Lenguaje de marcado para la estructura y elaboración de páginas web.</li>
+                            <li><b>CSS</b>: Lenguaje de diseño gráfico para definir el Estilo y diseño de un documento HTML.</li>
+                            <li><b>JavaScript</b>: Lenguaje de programación de secuencias de comandos</li>
 
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
+                        </ul>
+                        <div class="imgBloque1">
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+                            <Image src={imgVisualStudioCode} alt="Visual Studio Code"  />
+                            <Image src={imgGitSCM} alt="Git-SCM"  />
+                            <Image src={imgGitHub} alt="Github"  />
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
-}
+                        </div>
+                        <ul>
+                            <li><b>Visual Studio Code</b>: Editor de código fuente</li>
+                            <li><b>Git:</b>Sistema de control de versiones para el registro de los cambios de un proyecto.</li>
+                            <li><b>GitHub</b>: Plataforma de Desarrollo para alojar proyectos utilizando el sistema de control de
+                                versiones Git.</li>
+                        </ul>
+                        
+
+
+                    </div>
+                </div>
+            </div>
+
+
+
+        </Layout >
+    )
+} 
